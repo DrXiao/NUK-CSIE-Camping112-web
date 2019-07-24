10553a575de1186029a846da51f6d587f0180427
蕭 于恩 <zxc71230@gmail.com>
2019-07-24 12:38:24 +0800

    Second test

--- myweb.py
+++ myweb.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     if request.method == 'POST':
         return redirect(url_for('login',account = request.values['Account'],password = request.values['Password']))
-    
+
     return render_template('homepage.html')
 
 @app.route('/login:<account>_and_<password>')
@@ -18,6 +18,14 @@ def login(account,password):
     elif test == False:
         return '你輸入錯囉!'
 
+@app.route('/registration',methods = ['GET','POST'])
+def register():
+    if request.method == 'POST':
+        Account.register_new(request.values['Account'],request.values['Password'])
+        return '註冊成功!'
+    
+    return render_template('registration.html')
+
 if __name__ == '__main__':
-    app.debug = True
+    #app.debug = True
     app.run()

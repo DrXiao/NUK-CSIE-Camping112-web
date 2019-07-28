7695331191eabdab2c5572361cd6a56ce34eaae1
蕭 于恩 <zxc71230@gmail.com>
2019-07-28 14:16:35 +0800

    Renew annotation

--- Account.py
+++ Account.py
@@ -1,5 +1,25 @@
+#自定義模組SQL_method，是操作資料庫的模組
+#匯入SQL_method，並在此簡寫為sql
 import SQL_method as sql
 
+"""
+登入判斷函式，必定回傳兩個值，回傳值組(字串,一維陣列)
+在myweb「根目錄路由」的  context,Person = login(request.values['Account'],request.values['Password'])
+這段程式碼，也就相當於
+context,Person = '字串',[]
+
+換言之，在login的函式判斷，確實有該帳號、密碼也輸入對的話
+context 變數會得到 '正確!' 的字串
+Person  變數會得到 list[i]，一個一維陣列，包含 (帳號,密碼,人名)
+
+否則，
+context 變數會得到 錯誤訊息  的字串
+Person  變數會得到 None     值
+
+list是陣列，在sql (SQL_method) 模組的 get_table_SQL方法得到二維陣列
+得到所有帳號的資訊
+在帳號裡面找有沒有 account 帳號，password 密碼對不對
+"""
 def login(account,password):
     if account == '' or password == '':
         return '帳號或密碼為空!',None
@@ -13,6 +33,8 @@ def login(account,password):
                 return 'Error! 你的密碼輸入錯誤!',None
     return '你沒有帳號喔~ 快去註冊',None
 
+
+#找到帳號資訊的函式，回傳一個帳號的所有資訊的陣列-->輔助myweb的 logined_page() 函式
 def find_account(account):
     list = []
     list = sql.get_table_SQL()
@@ -20,5 +42,6 @@ def find_account(account):
         if account == list[i][0]:
             return list[i]
 
+#註冊帳號的函式
 def register_new_account(Name,Account,Password):
     sql.insert_table_SQL(Account,Password,Name)
--- SQL_method.py
+++ SQL_method.py
@@ -1,6 +1,44 @@
+# psycopg2 模組，用來連結資料庫的模組，內含一堆方法
 import psycopg2
 
+"""
+SQL_method的所有函式，必定包含下面6行code，我分為前3行跟後3行
 
+conn = psycopg2.connect(database="d8ti03uqsns0a0", user="upsvubypqddslc", password="257462a430af3ef3323f2bfe71e0ae14bd5a97bcfed571564e47807fc00d2adf", host="ec2-54-243-193-59.compute-1.amazonaws.com", port="5432")
+cur = conn.cursor()
+cur.execute('')
+---------------------------------------------------------------------------------
+翻譯:
+conn    :   利用psycopg2的connect方法，連結到資料庫，參數我已經打好惹，不用再變
+cur     :   conn取得資料庫，利用cursor方法，返回資料庫的指標給cur
+            如此，cur才是真正一個可以操作資料庫的變數
+cur.execute('')   : cur利用execute方法操作資料庫，參數是字串，字串是SQL語法
+---------------------------------------------------------------------------------
+
+conn.commit()
+cur.close()
+conn.close()
+---------------------------------------------------------------------------------
+翻譯    :   關閉資料庫，沒有關，之前的動作都會無效而且會出問題
+---------------------------------------------------------------------------------
+
+利用這6行，加上自己的Python code，實現有關於資料庫的操作
+"""
+
+
+"""
+create_table_SQL()    :   創建表格函式
+只需執行一次，就可以在資料庫建立「一種表格」，不能重複建立相同種類的表格，否則會出錯
+
+現在我已經建立一種表格叫做 Member，放在一個public的地方(呃...應該是這個意思)，內容是
+account(帳號)，長度32個字元，不能為空，是一個鍵值(就是只能有一個這個帳號的意思)
+password(密碼)，長度32個字元，不能為空
+name(名字)，長度32個字元，不能為空
+
+利用execute內加入SQL語法的字串，建立表格
+
+建立成功後，就在終端機print出 'public table is created'，才知道有沒有建立成功
+"""
 def create_table_SQL():
     conn = psycopg2.connect(database="d8ti03uqsns0a0", user="upsvubypqddslc", password="257462a430af3ef3323f2bfe71e0ae14bd5a97bcfed571564e47807fc00d2adf", host="ec2-54-243-193-59.compute-1.amazonaws.com", port="5432")
     cur = conn.cursor()
@@ -14,6 +52,13 @@ def create_table_SQL():
     cur.close()
     conn.close()
 
+"""
+insert_table_SQL()      :   新增資料函式(新增帳號)
+
+利用SQL語法，新增一筆Member格式的資料，把它加入PSQL資料庫裡面
+
+在終端機print出 'New user is created' ，知道有沒有新建成功
+"""
 def insert_table_SQL(account,password,name):
     conn = psycopg2.connect(database="d8ti03uqsns0a0", user="upsvubypqddslc", password="257462a430af3ef3323f2bfe71e0ae14bd5a97bcfed571564e47807fc00d2adf", host="ec2-54-243-193-59.compute-1.amazonaws.com", port="5432")
     cur = conn.cursor()
@@ -23,6 +68,14 @@ def insert_table_SQL(account,password,name):
     cur.close()
     conn.close()
 
+"""
+print_table_SQL()       :   顯示資料函式
+
+自己測試BUG的函式，平時不會用
+
+利用SQL語法  "SELECT * FROM member"，取得有關member的所有資料
+讓rows 陣列去接住這些資料( rows = cur.fetchall() )，並一行一行print出來(for迴圈)
+"""
 def print_table_SQL():
     conn = psycopg2.connect(database="d8ti03uqsns0a0", user="upsvubypqddslc", password="257462a430af3ef3323f2bfe71e0ae14bd5a97bcfed571564e47807fc00d2adf", host="ec2-54-243-193-59.compute-1.amazonaws.com", port="5432")
     cur = conn.cursor()
@@ -34,6 +87,14 @@ def print_table_SQL():
     conn.commit()
     cur.close()
     conn.close()
+
+"""
+get_table_SQL()     :   返回資料的函式
+
+code 與  print_table_SQL()  大同小異
+
+-->回傳資料二維陣列
+"""
 
 def get_table_SQL():
     conn = psycopg2.connect(database="d8ti03uqsns0a0", user="upsvubypqddslc", password="257462a430af3ef3323f2bfe71e0ae14bd5a97bcfed571564e47807fc00d2adf", host="ec2-54-243-193-59.compute-1.amazonaws.com", port="5432")

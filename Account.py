1d88a6fecc7976efa642070608d46bd9198eab61
蕭 于恩 <zxc71230@gmail.com>
2019-08-09 16:00:29 +0800

    QRscan test

--- Account.py
+++ Account.py
@@ -2,24 +2,20 @@
 #匯入SQL_method，並在此簡寫為sql
 import SQL_method as sql
 
-"""
-登入判斷函式，必定回傳兩個值，回傳值組(字串,一維陣列)
-在myweb「根目錄路由」的  context,Person = login(request.values['Account'],request.values['Password'])
-這段程式碼，也就相當於
-context,Person = '字串',[]
+class Member:
+    Account = ''
+    Password = ''
+    Name = ''
+    def __init__(self,Account,Password,Name):
+        self.Account = Account
+        self.Password = Password
+        self.Name = Name
+    def change(self,Account,Password,Name):
+        self.Account = Account
+        self.Password = Password
+        self.Name = Name
 
-換言之，在login的函式判斷，確實有該帳號、密碼也輸入對的話
-context 變數會得到 '正確!' 的字串
-Person  變數會得到 list[i]，一個一維陣列，包含 (帳號,密碼,人名)
 
-否則，
-context 變數會得到 錯誤訊息  的字串
-Person  變數會得到 None     值
-
-list是陣列，在sql (SQL_method) 模組的 get_table_SQL方法得到二維陣列
-得到所有帳號的資訊
-在帳號裡面找有沒有 account 帳號，password 密碼對不對
-"""
 def login(account,password):
     if account == '' or password == '':
         return '帳號或密碼為空!',[]
@@ -33,6 +29,5 @@ def login(account,password):
                 return 'Error! 你的密碼輸入錯誤!',[]
     return '你沒有帳號喔~ 快去註冊',[]
 
-#註冊帳號的函式
 def register_new_account(Name,Account,Password):
     sql.insert_table_SQL(Account,Password,Name)
--- myweb.py
+++ myweb.py
@@ -2,46 +2,25 @@
 from flask import Flask,render_template,request,url_for,redirect
 
 #自定義的套件，匯入三個函式
-from Account import login,register_new_account
+from Account import login,register_new_account,Member
+
+from QRscan_method import detect
 
 # __name__ 目前執行的模組
 app = Flask(__name__)
 
-Person = []
+member = Member('','','')
+
 
 #裝飾器，app.route()，決定一個「路由」要做什麼事情
-"""
-當路由是「'/'」(根目錄)的時候，顯示首頁，且該路由可以接受前端的GET、POST請求
-若沒有接受到前端的任何請求，顯示首頁頁面
 
-如果接受到來自前端的'POST'請求，則讓前端登入帳號
-
-context 變數，表示登入的對錯資訊字串
-若context的字串是 '正確!'，那麼就利用logined_page函式，返回一個「個人頁面」
-否則，就對前端顯示client的錯誤資訊
-
-Person 陣列，表示從資料庫取得的所有人的帳號密碼資訊
-是一個二維陣列，取得的資訊會長這樣
-[
-(帳號,密碼,人名),
-(帳號,密碼,人名),
-.
-.
-.
-]
-
-Person[i]，表示取得第i個人的所有資訊(變成一維陣列)
-Person[i][0]，表示第i個人的帳號
-以此類推
-
-使用「循序搜尋法」，尋找該帳號，核對密碼，即為判斷登入成不成功的方式
-"""
 @app.route('/',methods = ['GET','POST'])
 def home():
     if request.method == 'POST':
         context,Person = login(request.values['Account'],request.values['Password'])
         if context == '正確!':
-            return render_template('person_page.html',Name = Person[2], Account = Person[0], Password = Person[1],QRCODE = '')
+            member.change(Person[0],Person[1],Person[2])
+            return render_template('person_page.html',Name = member.Name, Account = member.Account, Password = member.Password,QRCODE = '')
         else:
             return context
 
@@ -63,6 +42,10 @@ def registration_page():
     return render_template('registration.html')
 
 
+@app.route('/QRscan')
+def QRcode_Scan():
+    text = detect()
+    return render_template('person_page.html',Name = member.Name, Account = member.Account, Password = member.Password,QRCODE = text)
 
 # 當__name__ 等於 '__main__'時，運作該網站
 if __name__ == '__main__':

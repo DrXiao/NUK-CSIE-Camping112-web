8af9e91cf4cbc624cb190ba56b4a4d490c445bc1
蕭 于恩 <zxc71230@gmail.com>
2019-08-09 20:41:17 +0800

    Camera test

--- myweb.py
+++ myweb.py
@@ -3,10 +3,25 @@ from flask import Flask,render_template,request,url_for,redirect,Response
 
 #自定義的套件，匯入三個函式
 from Account import login,register_new_account,Member
-import cv2
+from camera import Camera
 app = Flask(__name__)
 
 member = Member('','','')
+
+@app.route('/QRscan')
+def QRcode_scan():
+    return render_template('test.html')
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 #裝飾器，app.route()，決定一個「路由」要做什麼事情
 

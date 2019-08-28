2ca65402a8a2580ee3a0337e761b6a9ae96dbc74
蕭 于恩 <zxc71230@gmail.com>
2019-08-28 22:25:28 +0800

    Renew web timezone

--- trade.py
+++ trade.py
@@ -8,7 +8,7 @@ def team_give_score(team,target,score):
     conn = psycopg2.connect(database="d8ti03uqsns0a0", user="upsvubypqddslc", password="257462a430af3ef3323f2bfe71e0ae14bd5a97bcfed571564e47807fc00d2adf", host="ec2-54-243-193-59.compute-1.amazonaws.com", port="5432")
     cur = conn.cursor()
     cur.execute(' Update team SET score = %d Where teamname = \'%s\';'%(Score+score,Teamname))
-    timestr = '%d/%d/%02d:%02d'%(localtime.tm_mon,localtime.tm_mday,localtime.tm_hour,localtime.tm_min)
+    timestr = '%d/%d/%02d:%02d'%(localtime.tm_mon,localtime.tm_mday,localtime.tm_hour+8,localtime.tm_min)
     cur.execute('insert into trade(time,team,target,amount)values(\'%s\',\'%s\',\'%s\',%d)'%(timestr,Teamname,target,score))
     conn.commit()
     cur.close()
@@ -26,7 +26,7 @@ class trade_record:
         for i in range(len(trade_list)):
             dict1 = {'date':trade_list[i][0],'team':trade_list[i][1],'target':trade_list[i][2],'score':trade_list[i][3]}
             self.trade_list.append(dict1)
-            if len(trade_list)>10:
+            if len(self.trade_list)>10:
                 break
         conn.commit()
         cur.close()

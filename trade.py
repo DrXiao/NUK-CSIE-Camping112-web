be97c6e21ab66836e744d89501fe685545ca8f20
蕭 于恩 <zxc71230@gmail.com>
2019-08-29 22:32:49 +0800

    Time renew

--- trade.py
+++ trade.py
@@ -3,12 +3,12 @@ import psycopg2
 import time
 
 def team_give_score(team,target,score):
-    localtime = time.localtime()
+    localtime = time.localtime(time.time()+8*3600)
     Teamname,Score = sql.get_team_table_SQL(team)
     conn = psycopg2.connect(database="d8ti03uqsns0a0", user="upsvubypqddslc", password="257462a430af3ef3323f2bfe71e0ae14bd5a97bcfed571564e47807fc00d2adf", host="ec2-54-243-193-59.compute-1.amazonaws.com", port="5432")
     cur = conn.cursor()
     cur.execute(' Update team SET score = %d Where teamname = \'%s\';'%(Score+score,Teamname))
-    timestr = '%d/%d/%02d:%02d'%(localtime.tm_mon,localtime.tm_mday,localtime.tm_hour+8,localtime.tm_min)
+    timestr = '%d/%d/%02d:%02d'%(localtime.tm_mon,localtime.tm_mday,localtime.tm_hour,localtime.tm_min)
     cur.execute('insert into trade(time,team,target,amount)values(\'%s\',\'%s\',\'%s\',%d)'%(timestr,Teamname,target,score))
     conn.commit()
     cur.close()
